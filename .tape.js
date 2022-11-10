35020a2e1bfe6b94b5031201b1637b60093150f7
João Gonçalves <jandretgoncalves@gmail.com>
2022-11-10 15:29:30 +0000

    feat: Add more testing with function values

--- .tape.js
+++ .tape.js
@@ -1,8 +1,24 @@
 module.exports = {
   'jagoncalves14/stylelint-no-ending-comma-rule': [
+    // test valid options
+    {
+      source: 'body { top: 0; }',
+      args: true,
+      warnings: 0
+    },
+    {
+      source: 'body { color: rgba(0, 0, 0, 0); }',
+      args: true,
+      warnings: 0
+    },
     // test the # of warnings
     {
       source: 'body { top: 0,; }',
+      args: true,
+      warnings: 1
+    },
+    {
+      source: 'body { color: rgba(0, 0, 0, 0),; }',
       args: true,
       warnings: 1
     },
@@ -14,10 +30,21 @@ module.exports = {
         'Delete ending `,` (jagoncalves14/stylelint-no-ending-comma-rule)',
       ]
     },
+    {
+      source: 'body { color: rgba(0, 0, 0, 0),; }',
+      args: true,
+      warnings: [
+        'Delete ending `,` (jagoncalves14/stylelint-no-ending-comma-rule)',
+      ]
+    },
     // test autofixes
     {
       source: 'body { top: 0,; }',
       expect: 'body { top: 0; }'
+    },
+    {
+      source: 'body { color: rgba(0, 0, 0, 0),; }',
+      expect: 'body { color: rgba(0, 0, 0, 0); }',
     }
   ]
 }
